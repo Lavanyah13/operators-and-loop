f6bd8e421cc45283243196314b61704320ffa7f2
Lavanyah B <104270776+Lavanyah13@users.noreply.github.com>
2022-12-28 11:10:37 +0530

    Update Binary_to_Decimal.java

--- Binary_to_Decimal.java
+++ Binary_to_Decimal.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
-public class Solution{
-  public static void main {
+public class Main {
+  public static void main (String [] args){
     Scanner s=new Scanner(System.in);
     int num =s.nextInt();
     int dec_value = 0;
     int base=1;
-    temp=num;
+    int temp=num;
+    
     while (temp>0){
       int last_digit = temp%10;
-      temp = temm/10;
-      dec_value+ = last_digit *10;
+      temp = temp/10;
+      dec_value = last_digit *10;
       base = base *2;
-  ]
+    }
     System.out.println(dec_value);
       }
   }
-    
